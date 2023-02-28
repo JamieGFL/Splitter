5a71860dbff8fad68e2a8c52c27d4c8bc1afe561
Jamie Gerlach <jamie.gerlach1337@gmail.com>
2023-02-28 12:53:54 +0100

    add Person entity to domain

---
+++ splitter/src/main/java/propra2/splitter/domain/Person.java
@@ -0,0 +1,28 @@
+package propra2.splitter.domain;
+
+public class Person {
+    String name;
+    List<Expense> expenses;
+
+    public Person(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Expense> getExpenses() {
+        return expenses;
+    }
+
+    public void setExpenses(List<Expense> expenses) {
+        this.expenses = expenses;
+    }
+}
+
+
