e35bd3a9cd48041766aa2ad9360f4339a4d2b1de
Jamie Gerlach <jamie.gerlach1337@gmail.com>
2023-02-28 12:46:52 +0100

    add Group entity to domain

---
+++ splitter/src/main/java/propra2/splitter/domain/Group.java
@@ -0,0 +1,45 @@
+package propra2.splitter.domain;
+
+public class Group {
+    Integer id;
+    List<Person> persons;
+    Integer size = 0;
+    boolean closed = false;
+
+    public Group(Integer id, List<Person> persons) {
+        this.id = id;
+        this.persons = persons;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public List<Person> getPersons() {
+        return persons;
+    }
+
+    public void setPersons(List<Person> persons) {
+        this.persons = persons;
+    }
+
+    public Integer getSize() {
+        return size;
+    }
+
+    public void setSize(Integer size) {
+        this.size = size;
+    }
+
+    public boolean isClosed() {
+        return closed;
+    }
+
+    public void setClosed(boolean closed) {
+        this.closed = closed;
+    }
+}
