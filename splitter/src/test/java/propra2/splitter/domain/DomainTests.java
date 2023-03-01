fa75fa08acb3a63939c21e2cf1d95eb786324cec
Jamie Gerlach <jamie.gerlach1337@gmail.com>
2023-03-01 09:29:38 +0100

    create DomainTests class

---
+++ splitter/src/test/java/propra2/splitter/domain/DomainTests.java
@@ -0,0 +1,16 @@
+package propra2.splitter.domain;
+
+import org.javamoney.moneta.Money;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.atLeastOnce;
+import static org.mockito.Mockito.verify;
+
+public class DomainTests {
+
+
+}
