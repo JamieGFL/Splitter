aa505edb542a61cc4baf596ff899ccbb94e4877c
Jamie Gerlach <jamie.gerlach1337@gmail.com>
2023-03-01 09:13:33 +0100

    refactor Person into record

--- splitter/src/main/java/propra2/splitter/domain/Person.java
+++ splitter/src/main/java/propra2/splitter/domain/Person.java
@@ -1,30 +1,11 @@
 package propra2.splitter.domain;
 
+import org.javamoney.moneta.Money;
+
 import java.util.List;
 
-public class Person {
-    String name;
-    List<Expense> expenses;
+public record Person(String name, List<Expense> expenses, Money totalExpense) {
 
-    public Person(String name) {
-        this.name = name;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public List<Expense> getExpenses() {
-        return expenses;
-    }
-
-    public void setExpenses(List<Expense> expenses) {
-        this.expenses = expenses;
-    }
 }
 
 
