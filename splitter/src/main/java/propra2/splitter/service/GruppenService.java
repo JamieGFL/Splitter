0483fcabd8d2991855209f589b666ab01e7a1d31
PTamoyensis <Erik.Gal@hhu.de>
2023-03-06 11:51:51 +0100

    add addGruppe method

--- splitter/src/main/java/propra2/splitter/service/GruppenService.java
+++ splitter/src/main/java/propra2/splitter/service/GruppenService.java
@@ -1,2 +1,28 @@
-package propra2.splitter.service;public class GruppenService {
+package propra2.splitter.service;
+
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.stereotype.Service;
+import propra2.splitter.domain.Gruppe;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class GruppenService {
+
+    private List<Gruppe> gruppen = new ArrayList<>();
+
+    private void add(Gruppe gruppe){
+        gruppen.add(gruppe);
+    }
+
+    public Gruppe addGruppe(OAuth2User principle){
+        String login = principle.getAttribute("login");
+        Gruppe gruppe = Gruppe.erstelleGruppe(login);
+        add(gruppe);
+        return gruppe;
+    }
+
+    
+
 }
