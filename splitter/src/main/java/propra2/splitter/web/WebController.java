c217028e75812888f2bf01d0e03921e306ff8234
PTamoyensis <Erik.Gal@hhu.de>
2023-03-06 11:36:01 +0100

    add WebController and first template

---
+++ splitter/src/main/java/propra2/splitter/web/WebController.java
@@ -0,0 +1,16 @@
+package propra2.splitter.web;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class WebController {
+
+    @GetMapping("/")
+    public String gruppenSeite(){
+        return "index";
+    }
+
+
+}
