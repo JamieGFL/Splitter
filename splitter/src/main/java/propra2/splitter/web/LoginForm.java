688b1885afc2ebcce177c27e2f008aa7bc902360
Iraj Masoudian <itachimasoudian@gmail.com>
2023-03-07 10:16:31 +0100

    add LoginForm

---
+++ splitter/src/main/java/propra2/splitter/web/LoginForm.java
@@ -0,0 +1,21 @@
+package propra2.splitter.web;
+
+import jakarta.validation.constraints.Pattern;
+
+public class LoginForm {
+
+    @Pattern(regexp = "^[a-zA-Z0-9_-]{3,15}$", message = "Invalider Githubname")
+    private String login;
+
+    public LoginForm(String login) {
+        this.login = login;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+}
