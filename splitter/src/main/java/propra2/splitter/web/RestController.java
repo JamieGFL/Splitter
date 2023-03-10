410f39d517059b4cb27a8f3663f2e77f383a55ca
Jamie Gerlach <jamie.gerlach1337@gmail.com>
2023-03-10 13:24:01 +0100

    add addGruppen functionality for RestController

--- splitter/src/main/java/propra2/splitter/web/RestController.java
+++ splitter/src/main/java/propra2/splitter/web/RestController.java
@@ -1,23 +1,36 @@
 package propra2.splitter.web;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import propra2.splitter.service.GruppenOnPage;
 import propra2.splitter.service.GruppenService;
 
-@org.springframework.web.bind.annotation.RestController
-public class RestController {
+import java.util.UUID;
 
-    private final GruppenService service;
+    @org.springframework.web.bind.annotation.RestController
+    public class RestController {
 
-    public RestController(GruppenService service) {
-        this.service = service;
-    }
+        private final GruppenService service;
 
-//    @PostMapping("/api/gruppen")
-//    public ResponseEntity addGruppen(@RequestBody LoginForm loginForm) {
-//
-//        service.addGruppe()
-//        return;
-//    }
+        public RestController(GruppenService service) {
+            this.service = service;
+        }
+
+        @GetMapping("/api/user/{login}/gruppen")
+        public String gruppenSeite(@PathVariable String login){
+
+            return "index";
+        }
+
+      @PostMapping("/api/gruppen")
+      public ResponseEntity<UUID> addGruppen(@RequestBody GruppenForm gruppenForm) {
+        return new ResponseEntity<>(service.addRestGruppe(gruppenForm.getName(), gruppenForm.getPersonen()), HttpStatus.CREATED);
+        }
+
 }
