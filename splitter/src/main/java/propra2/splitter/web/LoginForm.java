f309e28ea40313cdc574dd801aeab210c943528f
Jamie Gerlach <jamie.gerlach1337@gmail.com>
2023-03-10 12:06:14 +0100

    initialize RestController

--- splitter/src/main/java/propra2/splitter/web/LoginForm.java
+++ splitter/src/main/java/propra2/splitter/web/LoginForm.java
@@ -2,13 +2,22 @@ package propra2.splitter.web;
 
 import jakarta.validation.constraints.Pattern;
 
+import java.util.List;
+
 public class LoginForm {
 
     @Pattern(regexp = "^[a-zA-Z0-9_-]{3,15}$", message = "Invalider Githubname")
     private String login;
+    private String name;
+    private List<String> personen;
 
     public LoginForm(String login) {
         this.login = login;
+    }
+
+    public LoginForm(String name, List<String> personen){
+        this.name = name;
+        this.personen = personen;
     }
 
     public String getLogin() {
