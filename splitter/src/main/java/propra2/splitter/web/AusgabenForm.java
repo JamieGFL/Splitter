7eccf0499eaabab109ea47fe6e423a80527427c7
Iraj Masoudian <itachimasoudian@gmail.com>
2023-03-13 09:28:14 +0100

    add validation to AusgabenForm

--- splitter/src/main/java/propra2/splitter/web/AusgabenForm.java
+++ splitter/src/main/java/propra2/splitter/web/AusgabenForm.java
@@ -1,11 +1,22 @@
 package propra2.splitter.web;
 
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+
 public class AusgabenForm {
 
-
+  @NotNull
+  @NotEmpty
   private final String aktivitaet;
+
   private final String zahler;
+
+  @NotNull
+  @NotEmpty
   private final String teilnehmer;
+  @NotNull
+  @Min(0)
   private final Double betrag;
 
   public AusgabenForm(String aktivitaet, String zahler, String teilnehmer, Double betrag) {
