7c7aaf3551cc2b8a75b59cc523899d0ee66de6d4
Iraj Masoudian <itachimasoudian@gmail.com>
2023-03-14 09:22:24 +0100

    add ArchTests and Stereotypes

--- splitter/src/test/java/propra2/splitter/ArchTests.java
+++ splitter/src/test/java/propra2/splitter/ArchTests.java
@@ -1,12 +1,20 @@
 package propra2.splitter;
 
+import com.tngtech.archunit.core.importer.ImportOption;
 import com.tngtech.archunit.junit.AnalyzeClasses;
 import com.tngtech.archunit.junit.ArchTest;
 import com.tngtech.archunit.lang.ArchRule;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Service;
 
+import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.classes;
+import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.noFields;
 import static com.tngtech.archunit.library.Architectures.onionArchitecture;
 
-@AnalyzeClasses(packagesOf = SplitterApplication.class)
+@AnalyzeClasses(packagesOf = SplitterApplication.class , importOptions = ImportOption.DoNotIncludeTests.class)
 public class ArchTests {
 
     @ArchTest
@@ -17,4 +25,15 @@ public class ArchTests {
             .adapter("web", "propra2.splitter.web..");
 
 
+    @ArchTest
+    static final ArchRule rule1 = noFields().should().beAnnotatedWith(Autowired.class).orShould().beAnnotatedWith(Value.class);
+
+    @ArchTest
+    static final ArchRule rule2 = classes().that().areAnnotatedWith(Configuration.class).should().resideInAPackage("..config..");
+
+    @ArchTest
+    static final ArchRule rule3 = classes().that().areAnnotatedWith(Service.class).should().resideInAPackage("..service..");
+
+    @ArchTest
+    static final ArchRule rule4 = classes().that().areAnnotatedWith(Controller.class).should().resideInAPackage("..web..");
 }
