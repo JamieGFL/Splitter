1d8162bbc20335f790c476d9474d301be3f58ba7
Iraj Masoudian <itachimasoudian@gmail.com>
2023-03-15 12:28:14 +0100

    add spring data dependency and GruppeDTO

---
+++ splitter/src/main/java/propra2/splitter/database/GruppeDTO.java
@@ -0,0 +1,7 @@
+package propra2.splitter.database;
+
+import org.springframework.data.annotation.Id;
+
+public record GruppeDTO(@Id Integer id, String gruppenName) {
+
+}
--- splitter/src/main/java/propra2/splitter/database/SpringDataGruppeRepository.java
+++ splitter/src/main/java/propra2/splitter/database/SpringDataGruppeRepository.java
@@ -1,5 +1,9 @@
 package propra2.splitter.database;
 
-public interface SpringDataGruppeRepository {
+import java.util.List;
+import org.springframework.data.repository.CrudRepository;
 
+public interface SpringDataGruppeRepository extends CrudRepository<GruppeDTO, Integer> {
+
+  List<GruppeDTO> findAll();
 }
