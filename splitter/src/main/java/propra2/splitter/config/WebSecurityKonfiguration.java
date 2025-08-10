737d15501aaefd796862ff89eb5700b5756b214d
ixYozora <itachimasoudian@gmail.com>
2025-08-10 19:45:25 +0200

    exchange deprecated security config

--- splitter/src/main/java/propra2/splitter/config/WebSecurityKonfiguration.java
+++ splitter/src/main/java/propra2/splitter/config/WebSecurityKonfiguration.java
@@ -2,6 +2,7 @@ package propra2.splitter.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.web.SecurityFilterChain;
@@ -12,17 +13,19 @@ public class WebSecurityKonfiguration {
   @Bean
   public SecurityFilterChain configure(HttpSecurity chainbuilder) throws Exception {
     chainbuilder.authorizeHttpRequests(configurer -> configurer.anyRequest().authenticated())
-        .logout()
-        .clearAuthentication(true)
-        .invalidateHttpSession(true)
-        .deleteCookies("JSESSIONID")
-        .and()
-        .oauth2Login();
+            .logout(e -> e
+                    .clearAuthentication(true)
+                    .invalidateHttpSession(true)
+                    .deleteCookies("JSESSIONID"))
+            .oauth2Login(Customizer.withDefaults());
+
+
     return chainbuilder.build();
   }
 
+
   @Bean
   public WebSecurityCustomizer customizer() {
-    return web -> web.ignoring().antMatchers("/api/**");
+    return web -> web.ignoring().requestMatchers("/api/**");
   }
 }
